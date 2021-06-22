92bb0ff023c23f4f8ee2f440f7f54d64288ef3be
shuiguai417 <1805480640@qq.com>
2021-06-22 14:18:11 +0800

    shuiguai

--- domain/src/main/java/com/guigu/erp/domain/m/Apply.java
+++ domain/src/main/java/com/guigu/erp/domain/m/Apply.java
@@ -5,7 +5,9 @@ package com.guigu.erp.domain.m;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.List;
@@ -14,7 +16,7 @@ import java.util.List;
 @TableName("m_apply")
 public class Apply {
     @TableId("id")
-    private String id;
+    private int id;
     @TableField("apply_id")
     private String applyId;
     @TableField("product_id")
@@ -28,11 +30,15 @@ public class Apply {
     private String designer;
     private String remark;
     private String register;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField("register_time")
     private Date registerTime;
     private String checker;
     @TableField("check_suggestion")
     private String checkSuggestion;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField("check_time")
     private Date checkTime;
     @TableField("check_tag")
